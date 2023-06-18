9a2a873c74d43bb3a5fc02347b4147d4acd9c4c0
jtierno <43449706+JonathanTierno@users.noreply.github.com>
2023-06-18 12:42:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
 from flask import Flask
 app = Flask(__name__)
 
