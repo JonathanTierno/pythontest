2115236bbff73e61282788d92afe29513436c2d6
jtierno <43449706+JonathanTierno@users.noreply.github.com>
2023-06-18 12:58:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    url = "https://www.danston.com"
+    response = requests.get(url)
+    content = response.content
+    
+    # Crear un objeto BeautifulSoup con el contenido HTML de la página de listado de artículos
+    soup = BeautifulSoup(driver.content, 'html.parser')
+
+    # Encontrar todos los elementos de artículo en la página
+    article_elements = soup.find_all('div', class_='prod_item')
+
+    # Iterar sobre los elementos de artículo y extraer la información deseada
+    for article in article_elements:
+        name = article.find('span', itemprop = 'itemprop').text
+        sku = article.find('span', itemprop = 'sku').text
+        price = article.find('span', itemprop = 'pprecio').text
+
+        print('Nombre:', name)
+        print('SKU:', sku)
+        print('Precio:', price)
+        print('---')
